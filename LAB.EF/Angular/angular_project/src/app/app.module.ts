83bd72e85b07c8647dcae85a6190f78106fbd7d5
FMartine7i <frmmartinetti98@gmail.com>
2023-09-23 22:03:24 -0300

    Login terminado de nuevo

--- LAB.EF/Angular/angular_project/src/app/app.module.ts
+++ LAB.EF/Angular/angular_project/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+//Existing components
+
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/dashboard/home/home.component';
+import { SharedModule } from './components/shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+import { EmployeesComponent } from './components/dashboard/employees/employees.component';
+import { LayoutComponent } from './components/dashboard/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    EmployeesComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    HttpClientModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ LAB.EF/Angular/angular_project/src/app/components/login/login.component.ts
@@ -0,0 +1,58 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {MatSnackBar, MatSnackBarRef, MatSnackBarModule} from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  hide:boolean = true;
+  loading:boolean = false;
+
+  formLogin:FormGroup;
+
+  constructor( private fb:FormBuilder, private _snackBar: MatSnackBar, private router:Router){
+    this.formLogin = this.fb.group({
+      user_name: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  login() {
+    this.router.navigate(['dashboard/layout']); 
+  }
+
+  login_account(){
+    console.log(this.formLogin);
+
+    const user_name = this.formLogin.value.user_name;
+    const password = this.formLogin.value.password;
+
+    if(user_name == 'admin' && password == '123'){
+      this.fake_redirection()
+    }
+    else{
+      this.errorMessage();
+    }
+  }
+
+  fake_redirection(){
+    this.loading = true;
+    setTimeout(() => {
+      this.loading = false
+      this.router.navigate(['dashboard/layout'])
+    }, 2000)
+  }
+
+  errorMessage() {
+    this._snackBar.open('Usuario o contraseña inválidos', 'OK', {
+      duration: 3000,
+      horizontalPosition: "center",
+      verticalPosition: "bottom"
+      });
+    }
+  }
+
